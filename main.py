401903c3f845e5f8e371e9a2c23715885ac01c35
mmaxpwd <max0000mn@gmail.com>
2023-07-19 16:58:10 +0500

    Данные с двух сайтов сохраняются теперь на одну вкладку, а не на две.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 import openpyxl
+from openpyxl import load_workbook
 
 url_headers = {
     "Accept": "*/*",
@@ -12,7 +13,10 @@ url_headers = {
 data_turt = []
 monthlist = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
 citylist = ['norway/oslo', 'portugal/porto', 'montenegro/podgoroditsa', 'montenegro/bar',  'portugal/lisbon', 'canada/vancouver', 'argentina/buenos_aires', 'portugal/madeira', 'bulgaria/varna', 'russia/severodvinsk', 'new_zealand/auckland', 'serbia/belgrade', 'russia/moscow', 'czech_republic/prague']
-
+citylist2 = ['Oslo','Porto','Podgorica','Bar-Montenegro','Lisbon',
+        'Vancouver','Buenos-Aires','Funchal','Varna','Severodvinsk-Russia',
+        'Auckland','Belgrade','Moscow','Prague']
+pricelist = []
 
 def collect_data_turt():
     count = 0
@@ -52,7 +56,7 @@ def collect_data_turt():
         print(f'Обработан город № {count}')
 
     book = openpyxl.Workbook()  # создание файла
-    sheet_1 = book.create_sheet("Климатические данные")  # создание вкладки
+    sheet_1 = book.create_sheet("Данные")  # создание вкладки
     book.remove(book.active)  # удаление пустой вкладки
 
     headers = list(data_turt[0].keys()) #добавляем заголовки
@@ -66,10 +70,71 @@ def collect_data_turt():
     book.close()
     print("Файл записан, работа завершена")
 def collect_data_numb():
-    pass
+    count = 0
+    for city in citylist2:
+        url = f'https://www.numbeo.com/cost-of-living/in/{city}?displayCurrency=USD'
+        response = requests.get(url=url, headers=url_headers)
+        src = response.text
+        soup = BeautifulSoup(src, 'lxml')
+        beef = float(
+            soup.find('table', class_="data_wide_table new_bar_table").find_all('tr')[16].find('span').text.split()[0])
+        potato = float(
+            soup.find('table', class_="data_wide_table new_bar_table").find_all('tr')[21].find('span').text.split()[0])
+        beer = float(
+            soup.find('table', class_="data_wide_table new_bar_table").find_all('tr')[26].find('span').text.split()[0])
+        gasoline = float(
+            soup.find('table', class_="data_wide_table new_bar_table").find_all('tr')[35].find('span').text.split()[0])
+        basic = float(
+            soup.find('table', class_="data_wide_table new_bar_table").find_all('tr')[39].find('span').text.split()[0])
+        jeans = float(
+            soup.find('table', class_="data_wide_table new_bar_table").find_all('tr')[50].find('span').text.split()[0])
+        rent = float(
+            soup.find('table', class_="data_wide_table new_bar_table").find_all('tr')[57].find('span').text.split()[
+                0].replace(',', ''))
+        price_psm = float(
+            soup.find('table', class_="data_wide_table new_bar_table").find_all('tr')[60].find('span').text.split()[
+                0].replace(',', ''))
+        salary = float(
+            soup.find('table', class_="data_wide_table new_bar_table").find_all('tr')[63].find('span').text.split()[
+                0].replace(',', ''))
+        for i in range(12): #число нужно для того чтобы заполнять пустые строки при сохранении в файл
+            pricelist.append(
+                {
+                    "Город": city,
+                    "Говядина": beef,
+                    "Картофель": potato,
+                    "Пиво": beer,
+                    "Бензин": gasoline,
+                    "Джинсы": jeans,
+                    "Аренда 3-к": rent,
+                    "Коммунальные платежи": basic,
+                    "Цена за квадратного метра": price_psm,
+                    "Зарплата": salary
+
+                }
+            )
+        count += 1
+        print(f'Обработан город № {count}')
+
+    book = load_workbook('data.xlsx')
+    active_sheet = book.active #выбираем активную вкладку
+    headers = list(pricelist[0].keys())  # добавляем заголовки
+    for col_num, header in enumerate(headers, 7):# цифра семь это номер столбца с которого начинается запись
+        active_sheet.cell(row=1, column=col_num, value=header)
+
+
+        for row_num, row_data in enumerate(pricelist, 2):
+            for col_num, cell_value in enumerate(row_data.values(), 7): # цифра семь это номер столбца с которого начинается запись
+                active_sheet.cell(row=row_num, column=col_num, value=cell_value)
+
+    book.save('data.xlsx')
+    book.close()
+
+    print("Файл записан, работа завершена")
 
 def main():
     collect_data_turt()
+    collect_data_numb()
 
 if __name__=="__main__":
     main()
